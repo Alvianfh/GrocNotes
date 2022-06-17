0309b70ca35426a1411b887379993904932d808e
alvianfh <alvianfaizh@gmail.com>
2022-06-18 01:20:53 +0700

    Update Screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,9 @@ import 'package:flutter/services.dart';
 import 'package:notes/screen/biometric.dart';
 import 'package:notes/screen/home.dart';
 import 'package:notes/screen/splash.dart';
+import 'package:responsive_framework/responsive_wrapper.dart';
+import 'package:responsive_framework/utils/scroll_behavior.dart';
 import 'package:supercharged/supercharged.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,6 +26,19 @@ class MyApp extends StatelessWidget {
           scaffoldBackgroundColor: 'ECECEC'.toColor(),
         ),
         home: Splash(),
+        builder: (context, widget) => ResponsiveWrapper.builder(
+              BouncingScrollWrapper.builder(context, widget!),
+              maxWidth: 1200,
+              minWidth: 550,
+              defaultScale: true,
+              breakpoints: [
+                const ResponsiveBreakpoint.autoScaleDown(550, name: MOBILE),
+                const ResponsiveBreakpoint.autoScale(800, name: TABLET),
+                const ResponsiveBreakpoint.autoScale(1000, name: TABLET),
+                const ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+                const ResponsiveBreakpoint.autoScale(2460, name: "4K"),
+              ],
+            ),
         routes: {
           Splash.id: (context) => Splash(),
           biometric.id: (context) => biometric()
